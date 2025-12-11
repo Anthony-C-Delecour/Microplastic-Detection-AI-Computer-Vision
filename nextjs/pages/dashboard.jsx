cba31c7d1df8d38a775477e78f69abf899013e3f
Anthony-C-Delecour <67110027@kmitl.ac.th>
2025-12-11 18:25:19 +0700

    Updated

---
+++ nextjs/pages/dashboard.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../pages/navbar.jsx";
+import styles from "../styles/dashboard.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div style={{ margin: 0, padding: 0 }}>
+      <Navbar />
+    </div>
+  );
+}
